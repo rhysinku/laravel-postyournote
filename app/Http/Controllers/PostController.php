b0fb054e08820e8b051f76c57ebb7245ea8039a2
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-08-24 16:55:12 +0800

    add pagination in post and add some place holder

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
     public function index()
     {
         //
-        $posts = Post::all();
+        $posts = Post::paginate(6);
         return view('post.home' , compact('posts'));
     }
 
--- resources/views/post/home.blade.php
+++ resources/views/post/home.blade.php
@@ -5,12 +5,20 @@
     <div class="flex flex-wrap justify-evenly my-2 gap-2">
 
         @foreach ($posts as $post)     
-        <section class="max-w-xl w-full p-5 rounded-xl bg-blue-200">
-            <h2 class="font-bold text-3xl mb-2">{{$post->title}}</h2>
+        <section class="max-w-md w-full p-5 rounded-xl bg-blue-200">
+            <h2 class="font-bold text-3xl mb-1">{{$post->title}}</h2>
+            <span class="italic text-sm text-blue-400 mb-2">author</span>
             <p>{{$post->message}}</p>
+            <div class="mt-3">
+                {{$post->created_at->diffForHumans()}}
+            </div>
         </section>
         @endforeach
 
+    </div>
+
+    <div class="mt-5">
+        {{$posts->links()}}
     </div>
 
 </x-layout>
